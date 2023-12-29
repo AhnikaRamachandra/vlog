6ec65447c51e5bc57da48185f86c1649c6d423a5
ahnika <98440824+AhnikaRamachandra@users.noreply.github.com>
2023-12-29 18:45:26 +0530

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+       Montserrat:["Montserrat, sans-serif"] ,
+       Fraunces:["Fraunces, serif"]
+
+      }
+    },
+  },
+  plugins: [],
+}
+
